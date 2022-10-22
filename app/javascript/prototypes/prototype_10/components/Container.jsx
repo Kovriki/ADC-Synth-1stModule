92cb98262273fa839874eebca1dc51963b9c92bf
Kovriki <polinamazitova@gmail.com>
2022-10-22 17:09:44 +0300

    die

--- app/javascript/prototypes/prototype_10/components/Container.jsx
+++ app/javascript/prototypes/prototype_10/components/Container.jsx
@@ -4,9 +4,10 @@ import React, { Component } from 'react'
 import * as bassSettings from '../tunes/bass.js'
 import * as melodySettings from '../tunes/melody.js'
 
-import SC_Button from './SC_Button'
-import SC_ToggleButtonSet from './SC_ToggleButtonSet'
-import SC_Slider from './SC_Slider'
+
+import ToneSynth from '../modules/ToneSynth'
+import SC_Button from '../components/SC_Button'
+import SC_Slider from '../components/SC_Slider'
 
 let bassSynth
 let bassChorus
@@ -80,14 +81,26 @@ export default class Container extends Component {
     //
     Tone.Transport.start()
   }
-
-  handleValueChange = (property, value) => {
+///
+  handleBassValueChange = (property, value) => {
     const { bassSettings } = this.state
 
     if (property === 'synthType') {
       bassSynth.oscillator.type = value
       bassSettings.synth.oscillator.type = value
-    } else if (property === 'pingPongDelayWet') {
+    } else if (property === 'synthEnvelopeAttack') {
+      bassSynth.envelope.attack = value
+      bassSettings.synth.envelope.attack = value
+    } else if (property === 'synthEnvelopeDecay') {
+      bassSynth.envelope.decay = value
+      bassSettings.synth.envelope.decay = value
+    } else if (property === 'synthEnvelopeSustain') {
+      bassSynth.envelope.sustain = value
+      bassSettings.synth.envelope.sustain = value
+    } else if (property === 'synthEnvelopeRelease') {
+      bassSynth.envelope.release = value
+      bassSettings.synth.envelope.release = value
+
       bassPingPongDelay.wet.value = value
       bassSettings.pingPongDelay.wet = value
     } else if (property === 'chorusWet') {
@@ -99,11 +112,44 @@ export default class Container extends Component {
       bassSettings
     })
   }
+////
+
+  handleMelodyValueChange = (property, value) => {
+    const { melodySettings } = this.state
+
+    if (property === 'synthType') {
+      melodySynth.oscillator.type = value
+      melodySettings.synth.oscillator.type = value
+
+    } else if (property === 'synthEnvelopeAttack') {
+      melodySynth.envelope.attack = value
+      melodySettings.synth.envelope.attack = value
+    } else if (property === 'synthEnvelopeDecay') {
+      melodySynth.envelope.decay = value
+      melodySettings.synth.envelope.decay = value
+    } else if (property === 'synthEnvelopeSustain') {
+      melodySynth.envelope.sustain = value
+      melodySettings.synth.envelope.sustain = value
+    } else if (property === 'synthEnvelopeRelease') {
+      melodySynth.envelope.release = value
+      melodySettings.synth.envelope.release = value
+
+    } else if (property === 'pingPongDelayWet') {
+      melodyPingPongDelay.wet.value = value
+      melodySettings.pingPongDelay.wet = value
+    } else if (property === 'chorusWet') {
+      melodyChorus.wet.value = value
+      melodySettings.chorus.wet = value
+    }
+
+    this.setState({
+      melodySettings
+    })
+  }
+
 
   render() {
-    const { synthSettings, chorusSettings, pingPongDelaySettings } = this.state
-
-    const options = ['sine', 'square', 'sawtooth', 'triangle']
+    const { bassSettings, melodySettings } = this.state
 
     return (
       <div className="Container">
@@ -111,15 +157,25 @@ export default class Container extends Component {
           text="Art Design & Coding Community"
           handleClick={this.handleStart}
         />
-
-        <SC_ToggleButtonSet
-          name="Type"
-          options={options}
-          value={bassSettings.synth.oscillator.type}
-          property="synthType"
+        <p>bass</p>
+        <ToneSynth
+        settings = {bassSettings}
+        handleValueChange = {this.handleBassValueChange}
+        />
+        <p>melody</p>
+        <ToneSynth
+        settings = {melodySettings}
+        handleValueChange = {this.handleMelodyValueChange}
+        />
+        <SC_Slider
+          name="Chorus Wet"
+          min={0}
+          max={1}
+          step={0.01}
+          value={bassSettings.chorus.wet}
+          property="chorusWet"
           handleChange={this.handleValueChange}
         />
-
         <SC_Slider
           name="Delay Wet"
           min={0}
@@ -130,15 +186,7 @@ export default class Container extends Component {
           handleChange={this.handleValueChange}
         />
 
-        <SC_Slider
-          name="Chorus Wet"
-          min={0}
-          max={1}
-          step={0.01}
-          value={bassSettings.chorus.wet}
-          property="chorusWet"
-          handleChange={this.handleValueChange}
-        />
+
       </div>
     )
   }
